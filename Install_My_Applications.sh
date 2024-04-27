349ab7096502c7fda0be58f48947588b0b7a2eda
b-nagaj <bryce.nagaj@gmail.com>
2024-04-26 22:28:59 -0600

    Seperate logic into functions

--- Install_My_Applications.sh
+++ Install_My_Applications.sh
@@ -32,33 +32,42 @@ VERACRYPT="*veracrypt*.deb"
 # .tar package aliases
 JETBRAINS_TOOLBOX="*jetbrains-toolbox*.tar.gz"
 
-# update package manager
-sudo apt update
-sudo apt upgrade -y
+update_package_manager() {
+    sudo apt update
+}
 
-# install prerequisites
-sudo apt install $SNAP $CURL -y
+install_prerequisites() {
+    sudo apt install $SNAP $CURL -y
+}
 
-# install programming libraries
-curl -fsSL https://deb.nodesource.com/setup_22.x
-sudo apt install $C_PLUS_PLUS $MAKE $BOOST $MY_SQL $NODE -y
+install_programming_libraries() {
+    curl -fsSL https://deb.nodesource.com/setup_22.x
+    sudo apt install $C_PLUS_PLUS $MAKE $BOOST $MY_SQL $NODE -y
+}
 
-# install applications via package manager
-sudo add-apt-repository ppa:mdoyen/homebank
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install $VIM $THUNDERBIRD $HOMEBANK $SPOTIFY $SUBLIME $GNOME_TWEAKS $GIT -y
+install_applications() {
+    # package manager
+    sudo add-apt-repository ppa:mdoyen/homebank
+    curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d
+    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    update_package_manager
+    sudo apt install $VIM $THUNDERBIRD $HOMEBANK $SPOTIFY $SUBLIME $GNOME_TWEAKS $GIT -y
 
-# install applications via .deb
-sudo apt install libccid libpcre2-32-0 libwxbase3.2-1 libwxgtk3.2-1 pcscd
-sudo dpkg -i $DBEAVER $BITWARDEN $GOOGLE_CHROME $OBSIDIAN $VISUAL_STUDIO_CODE $VERACRYPT
+    # .deb packages
+    sudo apt install libccid libpcre2-32-0 libwxbase3.2-1 libwxgtk3.2-1 pcscd
+    sudo dpkg -i $DBEAVER $BITWARDEN $GOOGLE_CHROME $OBSIDIAN $VISUAL_STUDIO_CODE $VERACRYPT
 
-# install applications via .tar.gz
-sudo tar xzf $JETBRAINS_TOOLBOX
-cd $JETBRAINS_TOOLBOX_DIRECTORY
-./jetbrains-toolbox
+    # .tar archives
+    sudo tar xzf $JETBRAINS_TOOLBOX
+    cd $JETBRAINS_TOOLBOX_DIRECTORY
+    ./jetbrains-toolbox
+}
+
+update_package_manager
+install_prerequisites
+install_programming_libraries
+install_applications
 
 echo "done!"
